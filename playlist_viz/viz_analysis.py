50d864577814a29594ece551ba696dab955d3989
anachronism <mhli@wpi.edu>
2021-01-13 11:11:59 -0500

    Changed to properly use pandas functions for removing NaNs, added ability to look over multiple crates, importing other clustering methods

--- playlist_viz/viz_analysis.py
+++ playlist_viz/viz_analysis.py
@@ -5,8 +5,9 @@ Created on Tue Jan 12 18:46:12 2021
 @author: Max
 """
 
-from sklearn.cluster import SpectralClustering
+from sklearn.cluster import SpectralClustering,DBSCAN,AgglomerativeClustering
 from  sklearn.manifold import TSNE
+from sklearn.preprocessing import StandardScaler
 
 import numpy as np
 import pandas as pd
@@ -16,29 +17,39 @@ csv_folder = "playlist_csvs"
 csv_folder_out = "output_playlists"
 
 ### TODO: add script to run through all csvs in folder.
-test_pool =  "_crate_10_.csv" #"liked.csv"
+test_pool =  "_crate_10_.csv" #"liked.csv" #
 test_ex = "jul_2020_chance_encounters.csv"
 nPlaylists = 6
+crate_range = [10,11]#,11,12] #,12
+df_pool = pd.DataFrame()
 
-fid_pool = "/".join((csv_folder,test_pool))
+for ind in crate_range:
+    test_str = "_crate_"+str(ind)+"_.csv"
+    fid_pool = "/".join((csv_folder,test_str))
+    df_tmp = pd.read_csv(fid_pool)
+    df_pool = df_pool.append(df_tmp)
+
 fid_ex = "/".join((csv_folder,test_ex))
-
-df_pool = pd.read_csv(fid_pool)
+#df_pool = pd.read_csv(fid_pool)
 df_pool = df_pool.sample(frac=1)
 df_ex = pd.read_csv(fid_ex)
+print(df_pool.head())
 
 featuresPull = ['Danceability','Energy','Speechiness','Acousticness'
-                ,'Instrumentalness','Liveness','Valence','Loudness','Tempo'] # ,'Key'
+                ,'Instrumentalness','Liveness','Valence','Loudness','Tempo'] #'Key',,'Loudness','Tempo' ### TODO: scale loudness, tempo to uniform.
+scaler = StandardScaler()
 
 df_clust_pool = df_pool[featuresPull]
+df_clust_pool = df_clust_pool.dropna(axis='rows')
+np_clust_pool = scaler.fit_transform(df_clust_pool)
+
 df_clust_ex = df_ex[featuresPull]
 
-np_clust_pool = df_clust_pool.to_numpy()
-cleaningInds = np.where(np.isnan(np_clust_pool))
-uniqueInds = np.unique(cleaningInds[0])
-np_clust_pool = np.delete(np_clust_pool,(uniqueInds),axis=0)
+#np_clust_pool = df_clust_pool.to_numpy()
 
 #df_cluster.head()
+#Prescale inputs
+
 labels = df_clust_pool.columns.values
 toCluster = df_clust_pool.to_numpy()
 print(labels)
@@ -49,38 +60,32 @@ print(labels)
 #
 #g2 = sns.PairGrid(df_clust_ex)
 #g2.map(sns.scatterplot)
-
+print("HERE0")
 ## Dimensionality reduction.
 dimRedModel = TSNE()
 poolReduced = dimRedModel.fit_transform(np_clust_pool)
-
+sns.scatterplot(poolReduced[:,0],poolReduced[:,1])
+print("HERE1")
 ## Clustering.
 sc = SpectralClustering(n_clusters = nPlaylists,n_jobs=-1)
+#sc = AgglomerativeClustering(n_clusters = nPlaylists,n_jobs=-1)
+#sc = DBSCAN(n_jobs=-1)
 splitVals = sc.fit_predict(poolReduced)
+nPlaylistOut = np.unique(splitVals).size
+print("HERE2")
 
-
-for ind in range(0,nPlaylists):
+for ind in range(0,nPlaylistOut):
     idxTest = np.where(splitVals==ind)
     idxTest = idxTest[0]
     playlistOut = df_pool.iloc[idxTest]
     playName = "".join(("playlist",str(ind),".csv"))
     playlistOut.to_csv("/".join((csv_folder_out,playName)))
 
+### TODO: write cluster values back out to some usable variable so I can mess with visualization.
 sns.scatterplot(poolReduced[:,0],poolReduced[:,1],hue=splitVals)
 
 
 
 
-'''
-Danceability
-energy
-Loudness
-speachiness
-acousticness
-instrumentalness
-liveness
-valence
-tempo (?)
-'''
-#df_whole.head()
+
 
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 Max's Spotify Projects:
 =========================
 Playlist Viz: looking into dividing crate playlists into smaller playlists. Relatively simple, based off of some dimensionality reduction and some clustering/partitioning. Viz is currently simple but can experiment with it. Should try to pull together a few crate playlists and see what comes of it.
+https://stackoverflow.com/questions/5452576/k-means-algorithm-variation-with-equal-cluster-size
 
 Playlist continuation: Trying to make playlists like some of the ordered playlists I included, from liked songs csv or combined crates. Needs some sort of playlist characterization or genre characterization or something along those lines. 
 
+Discover Weekly Compilation: This one I might just do quick, find all playlists called discover weekly and put all the songs they have in another playlist lol.
